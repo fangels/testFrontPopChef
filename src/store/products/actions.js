0ea7c0372e04f31d936e83d907828eadf12890de
fangels <allan@ouispoon.fr>
2020-05-24 14:46:14 +0200

    add action for products pages

---
+++ src/store/products/actions.js
@@ -0,0 +1,56 @@
+import API from '../../config/axios'
+
+export const requestAllProducts = async () => {
+  try {
+    const request = await API.get('/private/products/all')
+    return request.data
+  } catch (error) {
+    return false
+  }
+}
+
+export const requestProduct = async payload => {
+  try {
+    const request = await API.get('/private/products', {
+      params: {
+        id: payload.productId,
+      },
+    })
+    return request.data
+  } catch (error) {
+    return false
+  }
+}
+
+export const requestCreateProduct = async payload => {
+  try {
+    const request = await API.post('/private/products', {
+      ...payload,
+    })
+    return request.data
+  } catch (error) {
+    return false
+  }
+}
+
+export const requestUpdateProduct = async payload => {
+  try {
+    const request = await API.put('/private/products', {
+      ...payload,
+    })
+    return request.data
+  } catch (error) {
+    return false
+  }
+}
+
+export const requestDeleteProduct = async payload => {
+  try {
+    const request = await API.delete('/private/products', {
+      ...payload,
+    })
+    return request.data
+  } catch (error) {
+    return false
+  }
+}
